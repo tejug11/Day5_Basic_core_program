ff169c4ff9caad5fc4a9117f18994f333b8db046
Tejal Gaikwad <you@example.com>
2022-09-14 17:16:33 +0530

    Added : Code to compute the Quotient and Remainder

---
+++ QuotientRemainder.java
@@ -0,0 +1,17 @@
+package com.bridgelab;
+
+import java.util.Scanner;
+
+public class QuotientRemainder {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1st number : ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter 2nd number : ");
+		int num2 = sc.nextInt();
+		int quotient = (num1 / num2);
+		int remainder = (num1 % num2);
+		System.out.println("Quotient is : " + quotient);
+		System.out.println("Remainder is : " + remainder);
+	}
+}
